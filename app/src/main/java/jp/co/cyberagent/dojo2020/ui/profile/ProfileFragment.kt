fb193b41ac07f975cb74facf4e3511a4950d13cc
asagi71 <n.tomo9613@gmail.com>
2020-09-04 16:17:35 +0900

    profileFragment & ViewModel & xmlの追加

---
+++ app/src/main/java/jp/co/cyberagent/dojo2020/ui/profile/ProfileFragment.kt
@@ -0,0 +1,39 @@
+package jp.co.cyberagent.dojo2020.ui.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import jp.co.cyberagent.dojo2020.R
+import jp.co.cyberagent.dojo2020.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment() {
+
+
+    private lateinit var binding: FragmentProfileBinding
+    private lateinit var viewModel: ProfileViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            viewModel.liveData.observe(viewLifecycleOwner) {
+                profileUserName.text = it.name
+                profileTotalTime.text = it.totalTime
+            }
+            reloadButton.setOnClickListener {
+                viewModel.fetchUserData()
+            }
+        }
+
+    }
+}
---
+++ app/src/main/java/jp/co/cyberagent/dojo2020/ui/profile/ProfileViewModel.kt
@@ -0,0 +1,26 @@
+package jp.co.cyberagent.dojo2020.ui.profile
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import jp.co.cyberagent.dojo2020.DI
+import jp.co.cyberagent.dojo2020.data.model.Profile
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+class ProfileViewModel(context: Context) : ViewModel() {
+    private val repository = DI.injectLocalDataSource(context)
+
+    private val mutableLiveData: MutableLiveData<Profile> = MutableLiveData()
+    val liveData: LiveData<Profile>
+        get() = mutableLiveData
+
+    fun fetchUserData() = viewModelScope.launch {
+        repository.fetchAll().collect {
+            val userData = Profile("abcde", "12345")
+            mutableLiveData.value = userData
+        }
+    }
+}
