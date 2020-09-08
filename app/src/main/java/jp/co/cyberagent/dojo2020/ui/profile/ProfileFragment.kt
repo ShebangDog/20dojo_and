c253ac9721e1348f1af74da0b205f86f55251a24
asagi71 <n.tomo9613@gmail.com>
2020-09-08 10:41:29 +0900

    profileFragment & ViewModelの修正

--- app/src/main/java/jp/co/cyberagent/dojo2020/ui/profile/ProfileFragment.kt
+++ app/src/main/java/jp/co/cyberagent/dojo2020/ui/profile/ProfileFragment.kt
@@ -6,22 +6,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import jp.co.cyberagent.dojo2020.R
 import jp.co.cyberagent.dojo2020.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
 
 
     private lateinit var binding: FragmentProfileBinding
-    private val viewModel: ProfileViewModel by viewModels()
+
+    private val viewModel: ProfileViewModel by viewModels {
+        ProfileViewModelFactory(this, Bundle(), requireContext())
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(inflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
