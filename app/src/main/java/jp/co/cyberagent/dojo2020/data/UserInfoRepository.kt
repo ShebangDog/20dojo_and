43c513436d9890b33321f431b6525f6c13a1ccc7
ShebangDog <e1n18088@oit.ac.jp>
2020-09-19 02:26:47 +0900

    UserInfoRepositoryをinjectできるようにする

--- app/src/main/java/jp/co/cyberagent/dojo2020/data/UserInfoRepository.kt
+++ app/src/main/java/jp/co/cyberagent/dojo2020/data/UserInfoRepository.kt
@@ -1,16 +1,23 @@
 package jp.co.cyberagent.dojo2020.data
 
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.scopes.ActivityScoped
 import jp.co.cyberagent.dojo2020.data.model.FirebaseUserInfo
 import jp.co.cyberagent.dojo2020.data.remote.auth.FirebaseAuthentication
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import javax.inject.Inject
 
 interface UserInfoRepository {
     fun fetchUserInfo(): Flow<FirebaseUserInfo?>
 }
 
-class DefaultUserInfoRepository : UserInfoRepository {
+@ActivityScoped
+class DefaultUserInfoRepository @Inject constructor() : UserInfoRepository {
 
     @ExperimentalCoroutinesApi
     override fun fetchUserInfo(): Flow<FirebaseUserInfo?> {
@@ -24,4 +31,13 @@ class DefaultUserInfoRepository : UserInfoRepository {
             FirebaseUserInfo(uid, name, uri)
         }
     }
+}
+
+@Module
+@InstallIn(ActivityComponent::class)
+abstract class UserInfoModule {
+
+    @ActivityScoped
+    @Binds
+    abstract fun bindUserInfoRepository(defaultUserInfoRepository: DefaultUserInfoRepository): UserInfoRepository
 }
