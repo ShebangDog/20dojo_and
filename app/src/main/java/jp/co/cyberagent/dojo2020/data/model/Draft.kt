b1f371a6c6422747b03cd4e1618c2e344acb91f5
ShebangDog <e1n18088@oit.ac.jp>
2020-09-19 05:13:38 +0900

    Draftを作るためのメソッドを作る

--- app/src/main/java/jp/co/cyberagent/dojo2020/data/model/Draft.kt
+++ app/src/main/java/jp/co/cyberagent/dojo2020/data/model/Draft.kt
@@ -1,9 +1,20 @@
 package jp.co.cyberagent.dojo2020.data.model
 
+import java.util.*
+
 data class Draft(
     val id: String,
     val title: String,
     val content: String,
     val startTime: Long,
     val category: Category
-)
+) {
+    companion object {
+        fun createDraft(title: String, content: String, category: Category): Draft {
+            val id = UUID.randomUUID().toString()
+            val startTime = System.currentTimeMillis()
+
+            return Draft(id, title, content, startTime, category)
+        }
+    }
+}
