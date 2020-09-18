de1f9838f3364b9da935d45bd1cc33d2b9116f97
ShebangDog <e1n18088@oit.ac.jp>
2020-09-19 02:24:33 +0900

    DBをinjectできるようにする

--- app/src/main/java/jp/co/cyberagent/dojo2020/data/local/db/ApplicationDataBase.kt
+++ app/src/main/java/jp/co/cyberagent/dojo2020/data/local/db/ApplicationDataBase.kt
@@ -1,8 +1,15 @@
 package jp.co.cyberagent.dojo2020.data.local.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
 import jp.co.cyberagent.dojo2020.data.local.db.category.CategoryDao
 import jp.co.cyberagent.dojo2020.data.local.db.category.CategoryEntity
 import jp.co.cyberagent.dojo2020.data.local.db.converter.AccountListConverter
@@ -13,6 +20,7 @@ import jp.co.cyberagent.dojo2020.data.local.db.memo.MemoDao
 import jp.co.cyberagent.dojo2020.data.local.db.memo.MemoEntity
 import jp.co.cyberagent.dojo2020.data.local.db.profile.ProfileDao
 import jp.co.cyberagent.dojo2020.data.local.db.profile.ProfileEntity
+import javax.inject.Singleton
 
 @TypeConverters(AccountListConverter::class, CategoryConverter::class)
 @Database(
@@ -27,4 +35,20 @@ abstract class ApplicationDataBase : RoomDatabase() {
     abstract fun profileDao(): ProfileDao
 
     abstract fun categoryDao(): CategoryDao
+}
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object ApplicationDataBaseModule {
+
+    @Singleton
+    @Provides
+    fun provideApplicationDataBase(
+        @ApplicationContext applicationContext: Context
+    ): ApplicationDataBase = Room.databaseBuilder(
+        applicationContext,
+        ApplicationDataBase::class.java,
+        "ca-memo-database"
+    ).build()
+
 }
