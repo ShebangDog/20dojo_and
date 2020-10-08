a9b058d7dc97c1d3ef1cb0fb9645cca8e89026bd
ShebangDog <e1n18088@oit.ac.jp>
2020-10-09 02:30:52 +0900

    カテゴリの選択、作成をBottomSheetで行えるようにする

--- app/src/main/java/jp/co/cyberagent/dojo2020/ui/widget/CustomBottomSheetDialog.kt
+++ app/src/main/java/jp/co/cyberagent/dojo2020/ui/widget/CustomBottomSheetDialog.kt
@@ -1,14 +1,19 @@
 package jp.co.cyberagent.dojo2020.ui.widget
 
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowManager
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.activityViewModels
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import jp.co.cyberagent.dojo2020.databinding.LayoutBottomSheetBinding
 import jp.co.cyberagent.dojo2020.ui.create.MemoCreateViewModel
+import kotlinx.android.synthetic.main.layout_bottom_sheet.*
+import kotlin.random.Random
 
 class CustomBottomSheetDialog : BottomSheetDialogFragment() {
     companion object {
@@ -26,9 +31,6 @@ class CustomBottomSheetDialog : BottomSheetDialogFragment() {
 
         binding = LayoutBottomSheetBinding.inflate(inflater)
 
-        binding.addCategoryEditText.requestFocus()
-        dialog?.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE)
-
         return binding.root
     }
 
@@ -37,13 +39,35 @@ class CustomBottomSheetDialog : BottomSheetDialogFragment() {
 
         with(binding) {
             val onClick: (View) -> Unit = {
-                val category = addCategoryEditText.text.toString()
+                val category = addCategoryTextField.editText?.text.toString()
 
                 memoCreateViewModel.addCategory(category)
-                dismiss()
             }
 
             addCategoryButton.setOnClickListener(onClick)
+
+            with(addCategoryTextField) {
+                editText?.doOnTextChanged { text, _, _, _ ->
+                    add_category_button.visibility =
+                        if (text?.length == 0) View.GONE else View.VISIBLE
+                }
+
+                setEndIconOnClickListener {
+                    val color = pickColor()
+
+                    editText?.background?.colorFilter = PorterDuffColorFilter(
+                        color,
+                        PorterDuff.Mode.SRC_IN
+                    )
+                }
+            }
+
         }
+    }
+
+    private fun pickColor(): Int {
+        fun genElement() = Random.nextInt(256)
+
+        return Color.rgb(genElement(), genElement(), genElement())
     }
 }
