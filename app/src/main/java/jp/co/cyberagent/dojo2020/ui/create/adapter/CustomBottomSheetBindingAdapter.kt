038aee47ca7599ca3b1ab58593900136232bb4f4
ShebangDog <e1n18088@oit.ac.jp>
2020-10-12 02:30:32 +0900

    styleをdatabinding越しで選べられるようにする

--- app/src/main/java/jp/co/cyberagent/dojo2020/ui/create/adapter/CustomBottomSheetBindingAdapter.kt
+++ app/src/main/java/jp/co/cyberagent/dojo2020/ui/create/adapter/CustomBottomSheetBindingAdapter.kt
@@ -3,6 +3,7 @@ package jp.co.cyberagent.dojo2020.ui.create.adapter
 import android.content.Context
 import android.content.res.ColorStateList
 import android.widget.LinearLayout
+import androidx.annotation.StyleRes
 import androidx.databinding.BindingAdapter
 import com.google.android.material.chip.Chip
 import com.google.android.material.chip.ChipDrawable
@@ -12,14 +13,22 @@ import jp.co.cyberagent.dojo2020.data.model.Category
 import jp.co.cyberagent.dojo2020.ui.home.adapter.visibleOrGone
 import jp.co.cyberagent.dojo2020.ui.widget.OnChipClickListener
 
+sealed class ChipType(@StyleRes val style: Int) {
+    object Action : ChipType(R.style.Widget_MaterialComponents_Chip_Action)
+    object Choice : ChipType(R.style.Widget_MaterialComponents_Chip_Choice)
+    object Entry : ChipType(R.style.Widget_MaterialComponents_Chip_Entry)
+    object Filter : ChipType(R.style.Widget_MaterialComponents_Chip_Filter)
+}
+
 @BindingAdapter("app:visibility")
 fun bindVisibility(linearLayout: LinearLayout, isVisible: Boolean) {
     linearLayout.visibility = visibleOrGone(isVisible)
 }
 
-@BindingAdapter("app:categories", "app:onCategoryChipClick")
+@BindingAdapter("app:chipType", "app:categories", "app:onCategoryChipClick")
 fun bindChips(
     chipGroup: ChipGroup,
+    type: ChipType,
     categorySet: Set<Category>?,
     onChipClickListener: OnChipClickListener
 ) {
@@ -27,7 +36,7 @@ fun bindChips(
     chipGroup.removeAllViews()
     categorySet?.forEach { category ->
         chipGroup.addView(
-            createChip(chipGroup.context, category) {
+            createChip(chipGroup.context, type.style, category) {
                 onChipClickListener.onClick(it)
             },
             0
@@ -37,6 +46,7 @@ fun bindChips(
 
 private fun createChip(
     context: Context,
+    @StyleRes style: Int,
     category: Category,
     onChipClick: (Category) -> Unit
 ): Chip {
@@ -45,7 +55,7 @@ private fun createChip(
         context,
         null,
         0,
-        R.style.Widget_MaterialComponents_Chip_Choice
+        style
     )
 
     return chip.apply {
